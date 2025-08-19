13702943e19355fa83677d9f36e94d083da33b19
Pedro Peixoto <bonnieevil86@gmail.com>
2025-08-19 12:14:22 -0300

    Adding some comments and creating deleteUserSuccess test at userRepository

--- src/test/java/com/bonniezilla/aprendendospring/repositories/UserRepositoryTest.java
+++ src/test/java/com/bonniezilla/aprendendospring/repositories/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.bonniezilla.aprendendospring.repositories;
 
 import com.bonniezilla.aprendendospring.dtos.UserRequestDTO;
+import com.bonniezilla.aprendendospring.dtos.UserResponseDTO;
 import com.bonniezilla.aprendendospring.entities.User;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
@@ -25,23 +26,55 @@ class UserRepositoryTest {
     @Test
     @DisplayName("Should get user by id successfully from DB")
     void findUserByIdSuccess() {
+        // Instancing a "client request"
         UserRequestDTO data = new UserRequestDTO("user@test.com","UserTest");
 
+        // Requesting repository to create a test user
         User newUser = this.createUser(data);
 
+        // Try to get user by ID
         Optional<User> response = this.userRepository.findById(newUser.getId());
 
+        // Test if user exists
         assertThat(response.isPresent()).isTrue();
     }
 
     @Test
     @DisplayName("Shouldn't get user if don't exist")
     void findUserByIdFail() {
+        // Instancing a "client request"
         UserRequestDTO data = new UserRequestDTO("user@test.com","UserTest");
 
+        // Try to find a user with an impossible ID
         Optional<User> response = this.userRepository.findById(0L);
 
-        assertThat(response.isEmpty()).isTrue();
+        // Test if user with a not existent id can be found
+        assertThat(response.isEmpty()).as("Expect to don't find any user.").isTrue();
+
+    }
+
+    @Test
+    @DisplayName("Should delete user with success by his ID.")
+    void deleteUserSuccess() {
+        // Instancing a "client request"
+        UserRequestDTO data = new UserRequestDTO("user@test.com","UserTest");
+
+        // Requesting repository to create a test user
+        User newUser = this.createUser(data);
+
+        // Try to get user by ID
+        User dbUser = this.userRepository.findById(newUser.getId())
+                .orElseThrow(RuntimeException::new);
+
+        // Try to delete user founded
+        this.userRepository.delete(dbUser);
+
+        // Search deleted user by his ID
+        boolean exists = this.userRepository.existsById(dbUser.getId());
+
+        // Test if user exists after deleting him.
+        assertThat(exists).as("Expect to don't find user after trying to delete him. ").isFalse();
+
     }
 
     private User createUser(UserRequestDTO data) {
