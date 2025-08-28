2dc36ab6949775b7c661c2e5015fe8f4ff9bf560
Pedro Peixoto <bonnieevil86@gmail.com>
2025-08-28 19:14:08 -0300

    Little fix in a message error

--- src/main/java/com/bonniezilla/aprendendospring/dtos/UserRegisterDTO.java
+++ src/main/java/com/bonniezilla/aprendendospring/dtos/UserRegisterDTO.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserRegisterDTO(@NotBlank(message = "Username cannot be empty") String username,
-                              @Email(message = "Email should be valid") @NotBlank(message = "Email cannot be empyt") String email,
+                              @Email(message = "Email should be valid") @NotBlank(message = "Email cannot be empty") String email,
                               @NotBlank(message = "Password cannot be empty") String password) {}
